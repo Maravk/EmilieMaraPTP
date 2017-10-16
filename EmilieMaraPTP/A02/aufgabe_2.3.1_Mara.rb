2ddfdd940a72bc8f637529d7b8cbe806a0991712
Mara <maravauk@gmail.com>
2017-10-16 18:41:43 +0200

    changes

---
+++ EmilieMaraPTP/A02/aufgabe_2.3.1_Mara.rb
@@ -0,0 +1,13 @@
+def fib(n)
+  if n == 1
+    puts 0
+  else
+    fib = [0,1]
+    (n-2).times do
+      fib << fib[-1] + fib[-2]
+    end
+    puts fib.join(' ')
+  end
+end
+
+fib(500)
--- EmilieMaraPTP/A02/aufgabe_2.3_Mara.rb
+++ EmilieMaraPTP/A02/aufgabe_2.3_Mara.rb
@@ -1,8 +1,6 @@
 
-n = 0
-gets.chomp n
-n = n.to_i
-
 def fibo(n)
   n <= 2 ? 1 : fibo(n-1) + fibo(n-2)
 end
+
+puts fibo(100)
