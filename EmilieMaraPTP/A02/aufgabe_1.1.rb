575fe916ed4c33a1a8b0f4ea05f2c008300a467b
EmilieSarina <emilie.schuller@gmx.de>
2017-10-17 21:34:14 +0200

    aufgabe 1.1

--- EmilieMaraPTP/A02/aufgabe_1.1.rb
+++ EmilieMaraPTP/A02/aufgabe_1.1.rb
@@ -13,21 +13,23 @@ puts''
 puts "Ich freue mich, Sie hier anzutreffen, " + eingabe + ", Ihr Name ist sehr schön!"
 puts "Geht es Ihnen heute gut? Sofern sie glücklich sind, drücken sie die (1), und sofern heute nicht Ihr Tag ist, die (0)."
 eingabe = gets.chomp
-eingabe.to_i
+eingabe.to_s
+#puts eingabe
 
 #if-Schleife --> Je nach Eingabe des Nutzers wird auf der Konsole etwas Anderes ausgegeben.
-if eingabe != 0
-  then puts "Das klingt doch gut! Genießen Sie Ihren weiteren Tag!"
-if eingabe == 0
+if eingabe == "0"
   puts "Dann hoffe Ich, dass es Ihnen im Laufe des Tages besser gehen wird!!"
+else
+  puts "Das klingt doch gut! Genießen Sie Ihren weiteren Tag!"
+
 end 
-end 
+
 
 puts "Wie lautet denn Ihre Lieblingsfarbe?"
 eingabe = gets.chomp
 eingabe.to_i
 
-puts "Oh, interessant! Meine Lieblingsfarbe ist pink!Eine letzte Frage zum Abschluss noch: Wie alt (oder eher jung) sind Sie?"
+puts "Oh, interessant! Meine Lieblingsfarbe ist pink! Eine letzte Frage zum Abschluss noch: Wie alt (oder eher jung) sind Sie?"
 eingabe = gets.chomp
 eingabe.to_i
 
