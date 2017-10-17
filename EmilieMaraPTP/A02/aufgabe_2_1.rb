01eb3fc8864281841db59d66ad8a944744401a66
EmilieSarina <emilie.schuller@gmx.de>
2017-10-17 12:39:34 +0200

    Aufgabe 2.1 von Rico gemacht!!

---
+++ EmilieMaraPTP/A02/aufgabe_2_1.rb
@@ -0,0 +1,15 @@
+#Author: Emilie Sarina Schuller, Mara von Kroge, 12. Oktober 2017
+#Fibonacci-Folge
+#Variante 2.1
+class Aufgabe_2_1
+  
+  def fibonacci(f)
+    if f == 1
+      1
+    elsif f == 0
+      0
+    else
+      fibonacci(f-2)+fibonacci(f-1)
+    end
+  end
+end
