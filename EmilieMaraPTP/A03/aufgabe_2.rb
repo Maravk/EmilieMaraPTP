7ecdd81da996914909523d627a91667b040de8df
EmilieSarina <emilie.schuller@gmx.de>
2017-10-19 10:42:30 +0200

    Aufgabe_2

--- EmilieMaraPTP/A03/aufgabe_2.rb
+++ EmilieMaraPTP/A03/aufgabe_2.rb
@@ -20,18 +20,53 @@ def berechnung
 puts "Bitte geben Sie hier Ihre Kontonummer ein: "
 eingabe = gets.chomp
 eingabe = k
-k.to_s
+k.to_chars
+
+if k > 10
+  puts "Bitte geben Sie eine 10-stellige Kontonummer ein!"
+
 
 puts "Bitte geben Sie hier Ihre Bankleitzahl ein: "
 eingabe = gets.chono
 eingabe = b
-b.to_s
+b.to_chars
+
+if b > 8
+  puts "Bitte geben sie eine 8-stellige Bankleitzahl ein!"
 
 pruefziffer = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   
 puts "Ihre IBAN lautet wie folgt: #{k} #{b}"
 
-end
+  pruefziffer[0] = k[0]
+  pruefziffer[1] = k[1]
+  pruefziffer[2] = k[2]
+  pruefziffer[3] = k[3]
+  pruefziffer[4] = k[4]
+  pruefziffer[5] = k[5]
+  pruefziffer[6] = k[6]
+  pruefziffer[7] = k[7]
+  pruefziffer[8] = k[8]
+  pruefziffer[9] = k[9]
+  pruefziffer[10] = k[0]
+  pruefziffer[11] = b[1]
+  pruefziffer[12] = b[2]
+  pruefziffer[13] = b[3]
+  pruefziffer[14] = b[4]
+  pruefziffer[15] = b[5]
+  pruefziffer[16] = b[6]
+  pruefziffer[17] = b[7]
+  
+  
+  puts pruefziffer[0..17] + "1314 00"
+  
+  
+block 1 = pruefziffer[0..9]
+division = (block 1/97).to_i
+
+
+
+end 
   end 
 end 
 
