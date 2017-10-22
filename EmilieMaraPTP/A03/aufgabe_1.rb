5a3a6434b6fe2e5f89f2559725bf53d29b12dbb2
EmilieSarina <emilie.schuller@gmx.de>
2017-10-22 16:51:04 +0200

    aufgabe 1

---
+++ EmilieMaraPTP/A03/aufgabe_1.rb
@@ -0,0 +1,9 @@
+#Author:: Emilie Sarina chuller, Mara von Kroge, 18. Oktober 2017
+#Testfälle für Kreis, Rechteck und Dreieck
+# Aufgabe 1 - Testfälle schreiben
+
+require 'test/unit'
+require 'kreis'
+
+class Test_
+end
