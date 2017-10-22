af9bfb0afc45af442c2d1b0c1648adc012b642a8
EmilieSarina <emilie.schuller@gmx.de>
2017-10-22 13:57:03 +0200

    Iban-Test

--- EmilieMaraPTP/A03/iban_test.rb
+++ EmilieMaraPTP/A03/iban_test.rb
@@ -4,19 +4,20 @@
 require 'test/unit'
 require_relative 'iban_script'
 
-class Test_Iban < Test::Unit::TestCase
+class Test_iban < Test::Unit::TestCase
   def test_modulo
-    assert_equal(63, iban.new(nummer).modulo(97))
-    #iban_test1 = iban.new
-    #expected = iban_test1.nummer = nummer % 97
-   # assert_equal expected, 63
+    
+    iban_test1 = iban.new
+    expected = iban_test1.nummer = nummer % 97
+   assert_equal expected, -20
   end
 end
 
 
   def test_equal
-    assert_equal(063, iban.new(nummer).expected(063))
-#    iban_test2 = iban.new
-#    expected = iban_test2.nummer
-#    assert_equal expected, 063
+   
+    iban_test2 = iban.new
+    expected = iban_test2.nummer
+    assert_equal expected, 020
   end
+  
