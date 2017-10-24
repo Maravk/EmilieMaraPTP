6816ddb54e86eb2d6edec9afb549c655337b9199
Mara <maravauk@gmail.com>
2017-10-24 19:34:04 +0200

    new iban mara

--- EmilieMaraPTP/A03/iban_script_mara.rb
+++ EmilieMaraPTP/A03/iban_script_mara.rb
@@ -3,4 +3,16 @@
 
 require_relative 'iban_mara'
 
+# Begrüßung des Nutzers sowie kurze Einführung und Erklärung des Programmes.
+puts "Guten Tag! Dieses Programm wird Ihnen anhand Ihrer spezifischen Länderkennung,\nIhrer Kontonummer sowie Ihrer Bankleitzahl Ihre individuelle IBAN ermitteln!"
+
+# Leerzeichen für die Formatierung
+puts''
+
+iban = ""
 iban1 = Iban.new('DE', 54874843, 34543234)
+iban = iban1.return_iban
+puts "Fertige IBAN: " + iban
+iban1.check_this_iban(iban)
+
+puts "Fertige IBAN: " + iban
