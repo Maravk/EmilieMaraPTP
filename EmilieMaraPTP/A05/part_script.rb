7467cc0a217e9384b2454fba059da321a516aa79
EmilieSarina <emilie.schuller@gmx.de>
2017-11-21 19:18:27 +0100

    a05

---
+++ EmilieMaraPTP/A05/part_script.rb
@@ -0,0 +1,11 @@
+# Author: Mara von Kroge, Emilie Schuller, 21.11.2017
+# TeamChallenger
+# Script zur Part Klasse
+
+require_relative "part"
+include Enumerable
+
+
+my_list = Part.new("CPU", 0.01, "Computer")
+
+
