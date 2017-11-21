f596de6c4a135e8c3197365bb71783cea76403dc
EmilieSarina <emilie.schuller@gmx.de>
2017-11-21 20:04:34 +0100

    ruby ist scheiße :(

--- EmilieMaraPTP/A05/test_part.rb
+++ EmilieMaraPTP/A05/test_part.rb
@@ -3,11 +3,23 @@
 # Testklasse zu Teil
 
 require 'test/unit'
-require_relative 'teil_mara'
+require_relative 'part'
 
 class TestTeilMara < Test::Unit::TestCase
   
   def setup()
-    
+    @parts_computer = Part.new("Computer")
   end
+  
 end
+
+def test_initialize
+  part = Part.new("Part",1)
+  assert_equal("Part",part.name)
+  assert_equal(1,part.mass)
+  assert_equal(nil,part.parent)
+end
+
+
+#def test_teil_hinzufügen
+  
