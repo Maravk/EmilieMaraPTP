291618f14c811a8a40e1a642d5b22ed498f2fb56
Mara <maravauk@gmail.com>
2017-11-28 16:32:43 +0100

    part changes

--- EmilieMaraPTP/A05_version_2/part.rb
+++ EmilieMaraPTP/A05_version_2/part.rb
@@ -160,32 +160,4 @@ class Part
     @parts.push(new)
     return self
   end
-  
-  def write_to_file
-    filename = @name
-    puts filename
-    content = @name << "\n" << @parts.length.to_s << "\n" << @parts.flatten.to_s << "\n" << @mass.to_s << "\nParent: "
-    puts content
-    File.write(@name, "lol")
-  end
-  
-  def write_test
-    File.write(@name, @parts.flatten.to_s.gsub!(',', "\n").delete('[]" '))
-  end
-  
-  def load_from_file(filename)
-    line = IO.readlines(filename)[0]
-    @name = line
-    line = IO.readlines(filename)[1]
-    number_of_parts = line.to_i
-    line = IO.readlines(filename)[2]
-    words = line.split(/\W+/)
-    i = 0
-    array = 0
-    while (i < number_of_parts)
-      @parts[words[array]] = (@parts[words[array + 1]].to_f)
-      i = i+1
-      array = array + 2
-    end
-  end
 end
