98432430a4991c09821a65d77f579434b7f585c4
Mara <maravauk@gmail.com>
2017-11-29 11:34:03 +0100

    new set_parent

--- EmilieMaraPTP/A05_version_2/part.rb
+++ EmilieMaraPTP/A05_version_2/part.rb
@@ -6,11 +6,13 @@ class Part
   include Enumerable
   def initialize(name, mass, parent = nil)
     @name = name
-    @mass = 0.0
+    #    @mass = 0.0
     @mass = mass
     @parts = []
     @parent = parent
   end
+
+  # each
   
   def name()
     return @name
@@ -19,47 +21,48 @@ class Part
   def parent()
     return @parent
   end
-  
+
   def mass_of_this()
     return @mass
   end
-  
+
   def all_parts()
     return @parts
   end
-  
+
   def set_name(name)
     @name = name
   end
-  
+
   def set_mass(mass)
     @mass = Float(mass)
   end
-  
+
   def set_parent(parent)
+    parent.add_part.self
     @parent = parent
   end
-  
+
   # Entfernt ein Teil aus der Liste
   def remove(part)
-    if part == nil
+    if part.nil?
       raise IOError.new("Please remove a valid part!")
     end
     @parts.delete(part)
     return self
   end
-  
+
   #Gibt die Anzahl der Teile, aus denen ein Ganzes besteht, zurück, aber nicht mit der Anzahl Teile von Teilen
   def parts_amount()
     return @parts.size()
   end
-  
+
   def total_parts_amount()
     amount = self.parts_amount()
     @parts.each{|part| amount += part.total_parts_amount() }
     return amount
   end
-  
+
   #Wenn dieses Teil kein Gewicht hat, wird das Gesamtgewicht der Teile aus dem dieses besteht zurück gegeben.
   def mass()
     if(@mass == 0)
@@ -67,7 +70,7 @@ class Part
     end
     return @mass
   end
-  
+
   #Gibt die Masse des Ganzen zurück, falls es nicht noch aus anderen Teilen besteht
   #Sonst wird die Masse aller Teile und des Ganzen zurück gegeben
   def mass_of_all()
@@ -81,7 +84,7 @@ class Part
     mass += @mass
     return mass
   end
-  
+
   #trägt ein Objekt in den Array dieses Teils ein, in dem die Teile stehen aus dem dieses Teil besteht
   def add_part(part)
     if part == nil
@@ -91,16 +94,16 @@ class Part
     @parts.push(part)
     return self
   end
-  
-  # Jedes Einzelteil wird mit seiner jeweiigen Masse darstgestellt 
+
+  # Jedes Einzelteil wird mit seiner jeweiigen Masse darstgestellt
   def parts_and_mass()
     list = ""
     @parts.each { |value|
       list << value.name << ": " << value.mass_of_all.to_s << "\n"
     }
     return list
-    end
-  
+  end
+
   # Oerstes Teil
   def top()
     if(@parent.nil?)
@@ -108,12 +111,12 @@ class Part
     end
     return self.parent().top()
   end
-  
+
   # Prueft, ob der Array Teile enthaelt
   def empty?()
     return @parts.empty?()
   end
-  
+
   #Gibt Eigenschaften und Teile eines Ganzen in einer Tabelle aus
   def part_table()
     printf("Teil: %s     Masse: %s   |\n", sprintf("%-18s", @name), sprintf("%8d", self.mass))
@@ -125,14 +128,14 @@ class Part
     }
     puts "|_________________________________________________|"
   end
-  
+
   def remove(part)
     if part == nil
       raise TypeError.new("Please remove a valid part!")
     end
-      @parts.delete(part)
+    @parts.delete(part)
   end
-  
+
   # Ersetzt ein Teil aus der Liste
   def replace(part, new)
     if part == nil || new == nil
@@ -142,8 +145,8 @@ class Part
     new.parent = self
     @parts.push(new)
   end
-  
-    def replace(part, new)
+
+  def replace(part, new)
     if part == nil || new == nil
       raise TypeError.new("Please replace a valid part!")
     end
@@ -152,7 +155,7 @@ class Part
     @parts.push(new)
     return self
   end
-  
+
   def ==(part)
     if part.nil?
       return false
@@ -165,7 +168,7 @@ class Part
     end
     return [@name, @mass, @parent, @parts] == [part.name, part.mass_of_this, part.parent, part.all_parts]
   end
-  
+
   def eql?(part)
     if part.nil?
       return false
@@ -178,5 +181,5 @@ class Part
     end
     return [@name, @mass, @parent, @parts].eql?(part.name, part.mass_of_this, part.parent, part.all_parts)
   end
-  
+
 end
