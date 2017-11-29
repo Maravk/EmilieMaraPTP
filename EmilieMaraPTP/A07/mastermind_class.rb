cf372b8c389c11d192dfd3277918e719f378c589
EmilieSarina <emilie.schuller@gmx.de>
2017-11-29 08:31:11 +0100

    a07

---
+++ EmilieMaraPTP/A07/mastermind_class.rb
@@ -0,0 +1,11 @@
+# Author:: Mara von Kroge, Emilie Schuller
+# 29.11.2017
+# TeamChallenger
+# Klasse zu MasterMind
+
+class MasterMind
+  
+  def initialize(combinations=6)
+    @combinations = anzahl_moegliche_farben
+  end
+end
