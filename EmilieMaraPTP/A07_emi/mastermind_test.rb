c2dba8c35814422d5f11f0dc277fb35cf423b75c
EmilieSarina <emilie.schuller@gmx.de>
2018-01-15 12:38:54 +0100

    mastermind - tests

--- EmilieMaraPTP/A07_emi/mastermind_test.rb
+++ EmilieMaraPTP/A07_emi/mastermind_test.rb
@@ -11,17 +11,19 @@ class MastermindTest < Test::Unit::TestCase
   
   def setup()
     @mm = MastermindIO.new
-    @mara = Mastermind.new(4,6,10,self)
+    @mara = Mastermind.new(4,6,10,@mm)
   end
   
   def test_generate_code
     @emi = @mara.generate_code(4,6)
+    valid_elements = ["1","2","3","4","5","6"]
     assert_equal(4, @emi.length)
-    assert_equal((4.times(rand(1..6)).to_a), @emi)
+    assert_equal(true, @emi.each {|number| number.is_a?(String)})
+    assert_equal(true, @emi.each {|number| valid_elements.include?(number)})
   end
   
   def test_compare_codes()
-   @m = @mara.compare_codes([6,1,2,1],[3,4,2,6])
+    @m = @mara.compare_codes([6,1,2,1],[3,4,2,6])
     assert_equal([1,1],@m)
   end
 end
